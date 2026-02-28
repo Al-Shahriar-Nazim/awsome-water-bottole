6f7641faad5bbf32f531587d94b361a9905cae07
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-28 12:06:22 +0600

    added json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
 
 function App() {
+  // const bottol =[
+  //   {id:1,name:"pink nick bottol",prices:250,color:'pink'},
+  //   {id:2,name:"ywllow nick bottol",prices:270,color:'yellow'},
+  //   {id:3,name:"red nick bottol",prices:300,color:'red'},
+  //   {id:4,name:"black nick bottol",prices:600,color:'black'},
+  // ]
   return (
     <>
      <h1>Awsome water bottol</h1>
