d2e62f6fad45d81ebeb63562558fe2136430086a
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-03-01 19:36:54 +0600

    added project buy button

--- src/Components/Bottol/Bottol.jsx
+++ src/Components/Bottol/Bottol.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import "./bottol.css"
 
-const Bottol = ({bottol}) => {
+const Bottol = ({bottol,handleAddToCart}) => {
     console.log(bottol)
+    const{img,name,price,stock} = bottol;
     return (
-        <div>
-            
+        <div className='card bottol'>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>${price}</p>
+            <p>{stock} remaining</p>
+            <button onClick={()=>handleAddToCart(bottol)}>Buy now</button>
         </div>
     );
 };
--- src/Components/Bottols/Bottols.jsx
+++ src/Components/Bottols/Bottols.jsx
@@ -1,15 +1,25 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
+import "./bottols.css"
 import Bottol from "../Bottol/Bottol";
 
 const Bottols = ({ bottolsPromised }) => {
+    const[cart,setCart] = useState([])
+
   const bottols = use(bottolsPromised);
-  console.log(bottols);
+
+  const handleAddToCart =(bottol)=>{
+    // console.log("cart to cliked",bottol)
+    const newCart = [...cart,bottol];
+    setCart(newCart)
+  }
+//   console.log(bottols);
   return (
     <div>
       <h3>Total Bottols :{bottols.length}</h3>
-      <div>
+      <p>Add to cart :{cart.length}</p>
+      <div className="bottols-containers">
         {bottols.map((bottol) => (
-          <Bottol key={bottol.id} bottol={bottol}></Bottol>
+          <Bottol key={bottol.id} handleAddToCart={handleAddToCart} bottol={bottol}></Bottol>
         ))}
       </div>
     </div>
